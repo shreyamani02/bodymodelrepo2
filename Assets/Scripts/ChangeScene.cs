ac3a930cd5fb0d8c04a301662a21b6494bfe656d
Shreya Mani <72053004+shreyamani02@users.noreply.github.com>
2022-08-18 09:29:52 -0700

    Add files via upload

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void nextScene(string scenename)
+    {
+      SceneManager.LoadScene(scenename);
+    }
+}
